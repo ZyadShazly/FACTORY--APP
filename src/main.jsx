be8a4ccc99bad5405f497418808bf1f94325eed0
ZyadShazly <zyadshazly1@gmail.com>
2026-07-23 16:41:47 +0300

    fix: load procurement print styles globally

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./team-layout.css";
+import "./operational/procurementPrint.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
